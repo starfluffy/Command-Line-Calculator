7e08536adc0ab0461a5e1f7fb8d4dbc2cf4a0fa8
starfluffy <amandalowe868@gmail.com>
2022-07-15 13:36:35 +1200

    added operation function

--- main.c
+++ main.c
@@ -7,6 +7,13 @@ int getFirstNumber() {
 	return number1;
 }
 
+char getOperation() {
+	char opr;
+	printf("Enter the operation you would like to use: ");
+	scanf("%c", &opr);
+	return opr;
+}
+
 int getSecondNumber() {
 	double number2;
 	printf("Enter the second number of your calculation: ");
@@ -21,7 +28,17 @@ int main(double firstNumber, double secondNumber) {
 	double firstNumber = getFirstNumber();
 	double secondNumber = getSecondNumber();
 
-	if (operation == +) {
-		answer = 
+	if (operation == '+' ) {
+		answer = firstNumber + secondNumber;
+		return answer;
+	} else if (operation == '-') {
+		answer = firstNumber - secondNumber;
+		return answer;
+	} else if (operation == '*') {
+		answer = firstNumber * secondNumber;
+		return answer;
+	} else if (operation == '/') {
+		answer = firstNumber - secondNumber;
+		return answer;
 	}
 }
