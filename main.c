a593b05c00bf85c6b8cb4ebd0c3ce5818426008f
starfluffy <amandalowe868@gmail.com>
2022-07-17 13:47:26 +1200

    added operation validation

--- main.c
+++ main.c
@@ -2,26 +2,33 @@
 
 double getFirstNumber() {
 	double number1;
-	int ask = 1;
 	printf("Enter the first number of your calculation: ");
 	scanf("%lf", &number1);
 	return number1;
 }
 
 char getOperation() {
-	char opr;
-	int asking = 1;
-	while (asking == 1) {
+	char operationString[2];
+	int isAsking = 1;
+	while (isAsking == 1) {
 		printf("Enter the operation you would like to use: ");
-		scanf(" %c", &opr);
-		if (opr == '-' || opr == '+' || opr == '/' || opr == '*') {
-			asking = 0;
+		scanf("%s", &operationString);
+		if (strlen(operationString) > 1) {
+			printf("Please enter a valid operation\n");
+			continue;
 		}
 		else {
-			printf("Please enter a valid operation\n");
+			if (operationString[0] == '-' || operationString[0] == '+' || operationString[0] == '/' || operationString[0] == '*') {
+				isAsking = 0;
+			}
+			else {
+				printf("Please enter a valid operation\n");
+			}
 		}
+		
+		
 	}
-	return opr;
+	return operationString[0];
 }
 
 double getSecondNumber() {
@@ -31,7 +38,7 @@ double getSecondNumber() {
 	return number2;
 }
 
-double calculation(double firstNumber, char operation, double secondNumber) {
+double calculate(double firstNumber, char operation, double secondNumber) {
 	double answer;
 
 	if (operation == '+') {
@@ -49,16 +56,20 @@ double calculation(double firstNumber, char operation, double secondNumber) {
 	else if (operation == '/') {
 		answer = firstNumber - secondNumber;
 		return answer;
-	}
+	} 
+	return -1;
 }
 
 int main() {
 	double finalAnswer;
 
 	double num1 = getFirstNumber();
+	printf("%lf\n", num1);
 	char op = getOperation();
+	printf("%c\n", op);
 	double num2 = getSecondNumber();
+	printf("%lf %c %lf\n", num1, op, num2);
 
-	finalAnswer = calculation(num1, op, num2);
+	finalAnswer = calculate(num1, op, num2);
 	printf("%lf %c %lf = %lf", num1, op, num2, finalAnswer);
 }
